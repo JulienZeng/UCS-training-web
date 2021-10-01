1c615a76d22223f93e0f8b29afd9cad8863725ce
JulienZeng <936636589@qq.com>
2021-10-02 02:58:43 +0800

    尝试使用video标签

---
+++ src/views/anxiang.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h1>暗香：</h1>
+        <video width="440" height="200" controls>
+            <source src="http://zjlserver.site:3030/anxiang.mp4" type="video/mp4">
+        </video>
+    </div>
+</template>
---
+++ src/views/lingyin.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <br/>
+        <h1>泠音：</h1>
+        <br/>
+        <video width="440" height="200" controls>
+            <source src="http://zjlserver.site:3030/lingyin.mp4" type="video/mp4">
+        </video>
+    </div>
+</template>
