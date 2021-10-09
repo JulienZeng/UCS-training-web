e3ad26b5f8f23fad5fe0188238d93f8bd864bc6a
JulienZeng <936636589@qq.com>
2021-10-10 02:09:27 +0800

    学习网站链接

---
+++ src/views/webLink.vue
@@ -0,0 +1,24 @@
+<template>
+    <div style="display:flex;flex-direction:column;text-align:left">
+        <a href="http://www.baidu.com">百度</a><br>
+        <a href="http://cn.bing.com">必应</a><br>
+        <a href="http://www.google.com">谷歌</a><br>
+        <a href="http://www.csdn.net">CSDN</a><br>
+        <a href="http://www.runoob.com">菜鸟教程</a><br>
+        <a href="http://www.w3school.com.cn">W3 School</a>
+    </div>
+</template>
+
+<style scoped>
+a{
+  text-decoration:none;
+  color:blue;
+  font-size: 10rpx;
+}
+a:hover{
+  color:red;
+}
+a:active{
+  color: purple;
+}
+</style>
