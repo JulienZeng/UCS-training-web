ca9c9aa8f46ef69811263fa2db6eb69c6a333ae5
JulienZeng <936636589@qq.com>
2021-10-10 02:09:00 +0800

    添加vant组件库

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import installElementPlus from './plugins/element'
 
+//引入组件库 Vant
+import Vant from 'vant';
+import 'vant/lib/index.css';
+
 //定义页面标题
 function title(){
     document.title = '京涛海纳工作室-微博中心'
@@ -12,3 +16,5 @@ const app = createApp(App)
 installElementPlus(app)
 app.use(router).mount('#app')
 title()
+
+app.use(Vant);
