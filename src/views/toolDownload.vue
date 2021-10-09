246f7f484b6f2e468830030c3d6685be83f1a6e7
JulienZeng <936636589@qq.com>
2021-10-10 02:09:43 +0800

    资料下载

---
+++ src/views/toolDownload.vue
@@ -0,0 +1,62 @@
+<template v-slot:dropdown>
+    <div class="outside">
+        <div class="">
+            <van-cell-group inset>
+                <div class="main" v-for="(item,key) in downloads" :key="key">
+                    <!-- 左侧图片 -->
+                    <div class="photo" :style="{backgroundImage:'url('+ item.photoUrl + ')'}" style="background-size:cover"></div>
+                    <van-cell>
+                        <template #title>
+                            <!-- 介绍文字 -->
+                            <div class="itemsName">{{item.name}}</div>
+                        </template>
+                        <template #label>
+                            <div>{{item.shotIntroduction}}</div>
+                        </template>
+                        <template #value>
+                            <van-button round type="primary" @click="openUrl(item.downloadUrl)">&nbsp;&nbsp;下&nbsp;&nbsp;载&nbsp;&nbsp;</van-button>
+                        </template>
+                    </van-cell>
+                </div>
+                
+            </van-cell-group>
+        </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    data(){
+        return{
+            downloads:[
+                {
+                    photoUrl:require('../assets/vscode.png'),
+                    name:'Visual Studio Code',
+                    shotIntroduction:'一款强大且好用的编译器',
+                    mainIntroduction:'',
+                    downloadUrl:'http://zjlserver.site:3030/VSCodeUserSetup-x64-1.61.0.exe'
+                }
+            ]
+        }
+    },
+    methods:{
+        openUrl(url){
+            console.log(url);
+            window.open(url);
+        }
+    }
+}
+</script>
+
+<style scoped>
+.main{
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+}
+.photo{
+    height: 25rpx;
+    width: 25rpx;
+}
+</style>
